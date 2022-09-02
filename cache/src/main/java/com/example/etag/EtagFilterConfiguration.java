a8882dee7be0108f5499ab2ec8159342d1b39985
DOES <ldk980130@gmail.com>
2022-09-02 12:04:01 +0900

    feat: ETag/If-None-Match 적용하기

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,20 @@
 package com.example.etag;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+	private static final String ETAG_URL_PATH = "/etag";
+
+	@Bean
+	public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+		FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean =
+			new FilterRegistrationBean<>(new ShallowEtagHeaderFilter());
+		filterRegistrationBean.addUrlPatterns(ETAG_URL_PATH);
+		return filterRegistrationBean;
+	}
 }
